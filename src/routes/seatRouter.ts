f64def2091e75c6b984c8a4aa872a8b0471c187c
RoenissMoon <roeniss2@gmail.com>
2020-03-03 22:21:07 +0900

    add router templates

---
+++ src/routes/seatRouter.ts
@@ -0,0 +1,10 @@
+import * as express from "express";
+
+const router = express.Router();
+
+// router.post("/login", (req: express.Request, res: express.Response, next: express.NextFunction) => {
+//   console.log(req.body);
+//   return res.sendStatus(200);
+// });
+
+export default router;
