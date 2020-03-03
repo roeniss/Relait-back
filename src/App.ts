fb75ba64d910b67a3c3e90b55af12c5a17fbed62
RoenissMoon <roeniss2@gmail.com>
2020-03-03 22:41:23 +0900

    error -> status: 500

--- src/App.ts
+++ src/App.ts
@@ -23,7 +23,7 @@ class App {
     this.app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
       const env = process.env.NODE_ENV;
       if (env === "local" || env === "development") {
-        res.send(err);
+        res.status(500).send(err);
       } else {
         res.sendStatus(500);
       }
