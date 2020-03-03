838d281f0efbddce78e1fbf93e1e420ae1976281
RoenissMoon <roeniss2@gmail.com>
2020-03-03 22:43:46 +0900

    default port: 8080 -> 9000

--- src/www.ts
+++ src/www.ts
@@ -3,7 +3,7 @@ import "../env";
 import App from "./App";
 import * as express from "express";
 
-const port: number = Number(process.env.PORT) || 8080;
+const port: number = Number(process.env.PORT) || 9000;
 const app: express.Application = new App().app;
 
 app.listen(port, () => console.log(`Express server listening at ${port}`)).on("error", (err) => console.error(err));
