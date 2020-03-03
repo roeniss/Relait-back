ded6afc6489fb680e9fff6e27cf51f1ccf1fadfb
RoenissMoon <roeniss2@gmail.com>
2020-03-03 22:21:50 +0900

    edit app settings

--- src/App.ts
+++ src/App.ts
@@ -1,22 +1,32 @@
 import express from "express";
+import bodyParser from "body-parser";
+import Router from "./routes/index";
+import logger from "morgan";
 
 class App {
   public app: express.Application;
 
-  /**
-   * @ class App
-   * @ method bootstrap
-   * @ static
-   *
-   */
   public static bootstrap(): App {
     return new App();
   }
 
   constructor() {
     this.app = express();
+
+    this.app.use(logger("dev"));
+    this.app.use(bodyParser.json());
+    this.app.use("/", Router);
     this.app.get("/", (req: express.Request, res: express.Response, next: express.NextFunction) => {
       res.send("Hello world");
+    });
+
+    this.app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+      const env = process.env.NODE_ENV;
+      if (env === "local" || env === "development") {
+        res.send(err);
+      } else {
+        res.sendStatus(500);
+      }
     });
   }
 }
