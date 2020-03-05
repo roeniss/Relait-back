8321c6b4a49f8e08f54126311d7159c41f8ecbd7
RoenissMoon <roeniss2@gmail.com>
2020-03-05 14:44:56 +0900

    add token verify function

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -16,6 +16,8 @@ export interface User {
 interface JwtPayload {
   id: number;
   nickname: string;
+  iat?: number;
+  exp?: number;
 }
 
 export type Jwt = string;
@@ -31,4 +33,23 @@ export const makeJwt = (user: UserSchema): Jwt => {
   if (!jwtSecret) throw new Error("No JWT Secret");
   const token = jwt.sign(payload, jwtSecret, { expiresIn: process.env.JWT_EXPIRE || "14d" });
   return token;
+};
+
+const isObj = (target: any): boolean => typeof target === "object" && target !== null;
+
+export const getDataFromJwt = (token: Jwt): JwtPayload | null => {
+  try {
+    const jwtSecret = process.env.JWT_SECRET;
+    if (!jwtSecret) throw new Error("No JWT Secret");
+    const resultToken: string | object = jwt.verify(token, jwtSecret);
+    if (!isObj(resultToken)) return null;
+    const payload = <JwtPayload>resultToken;
+    const curTime = new Date().getDate() / 1000;
+    if (payload.exp && payload.exp - curTime > 0) return { id: 1, nickname: "2" };
+    else null;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+  return null;
 };
