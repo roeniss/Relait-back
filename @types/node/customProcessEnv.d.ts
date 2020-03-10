347d1ed2ca72da0af6491f57babacbd60d732a81
RoenissMoon <roeniss2@gmail.com>
2020-03-10 21:10:00 +0900

    login input 검사 내용 변경

--- @types/node/customProcessEnv.d.ts
+++ @types/node/customProcessEnv.d.ts
@@ -3,18 +3,20 @@ export {};
 declare global {
   namespace NodeJS {
     interface IProcessEnv {
-      // 인텔리센스말곤 딱히 해주는게 없다
-      NODE_ENV: string;
-      PEM: string;
-      DB_NAME: string;
-      DB_PASSWORD: string;
-      DB_HOST: string;
-      DB_DIALECT: string;
-      DB_REMOTE_HOST: string;
-      DB_REMOTE_USERNAME: string;
-      DB_USER: string;
-      JWT_SECRET: string;
-      PORT: string;
+      /**
+       * Environment variables used in current app
+       */
+      NODE_ENV?: string;
+      PEM?: string;
+      DB_NAME?: string;
+      DB_PASSWORD?: string;
+      DB_HOST?: string;
+      DB_DIALECT?: string;
+      DB_REMOTE_HOST?: string;
+      DB_REMOTE_USERNAME?: string;
+      DB_USER?: string;
+      JWT_SECRET?: string;
+      PORT?: string;
     }
     interface ProcessEnv extends IProcessEnv {}
   }
